a221d31143b643b452060d501a0ed8c0f1b79c23
Hanan Mohamud <152763595+hananmyy@users.noreply.github.com>
2024-11-08 17:16:36 +0300

    Add files via upload

---
+++ index.py
@@ -0,0 +1,26 @@
+# question 1
+my_list = []
+
+# question 2
+# 
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+# question 3
+my_list.insert(1, 15)
+
+# question 4
+my_list.extend([50, 60, 70])
+
+# question 5
+my_list.pop()
+
+# question 6
+my_list.sort()
+
+print("The list after all operations:", my_list)
+
+# question 7
+index_of_30 = my_list.index(30)
+print("The index of 30 in my_list is:", index_of_30)
